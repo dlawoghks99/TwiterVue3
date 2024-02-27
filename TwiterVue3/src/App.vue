547212d9f632da95cfe70bc63632c24e536171c4
JeaHwanLim <dlawoghks5708@gmail.com>
2024-02-27 15:59:51 +0900

    채팅목록 구현

--- TwiterVue3/src/App.vue
+++ TwiterVue3/src/App.vue
@@ -1,41 +1,41 @@
 <template>
-  <div class="flex h-screen">
+  <div class="flex h-screen container mx-auto">
     <!-- 사이드 섹션-->
-    <div class="w-1/4 pt-5 ml-10 flex flex-col justify-between border-r border-gray-200">
+    <div class="w-20 lg:w-1/4 pt-5 lg:ml-10 flex flex-col justify-between border-r border-gray-200">
       <div class="flex flex-col items-center lg:items-start">
         <!-- 트위터 로그-->
         <i class="fa-brands fa-twitter text-2xl text-primary lg:ml-4 mb-3"></i>
         <!-- 트위터 사이드메뉴 아이콘-->
         <div class="flex flex-col items-start">
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="fa-solid fa-house fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">홈</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="fa-solid fa-hashtag fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">탐색하기</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="far fa-bell fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">알림</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="far fa-envelope fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">쪽지</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="far fa-bookmark fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">북마크</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="far fa-list-alt fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">리스트</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="far fa-user fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">프로필</span>
           </div>
-          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
             <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
             <span class="ml-5 text-lg hidden lg:inline-block">더보기</span>
           </div>
@@ -63,69 +63,20 @@
           </div>
       </div>
     </div>
-    <div class="flex-1">
-      <div class="flex h-screen">
-        <!-- 메인 섹션-->
-        <div class="flex-1 border-r border-gray-100 overflow-y-auto">
-          <div class="flex flex-col">
-            <!-- 페이지 타이틀-->
-            <div class="border-b border-gray-100 px-3 py-2 font-bold text-lg">홈</div>
-            <!-- 트위팅 섹션-->
-            <div class="flex px-3 py-3 border-b-8 border-gray-200">
-              <img src="http://picsum.photos/200" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer" />
-              <div class="ml-2 flex-1 flex flex-col">
-                <textarea placeholder="무슨 일이 일어나고 있나요?"
-                class="w-full text-lg font-bold focus:outline-none mb-3 resize-none "></textarea>
-                <div class="text-right">
-                  <button class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
-                </div>
-              </div>
-            </div>
-            <!-- 트위츠-->
-            <div class="flex px-3 py-3 border-b border-gray-100" v-for="tweet in 10" :key="tweet">
-              <img src="http://picsum.photos/200" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer" />
-              <div class="ml-3 flex-1 flex flex-col space-y-1">
-                <div class="text-sm space-x-1">
-                  <span class="font-bold">임재환</span>
-                  <span class="text-gray-500 text-xs">@dlawoghks</span>
-                  <span>·</span>
-                  <span class="text-gray-500 text-xs">7분</span>
-                </div>
-                <!-- 트윗 body-->
-                <div>테스트입니다.</div>
-                <!-- 트윗 actions-->
-                <div class="flex justify-between">
-                  <div class="text-gray-500 hover:text-primary">
-                    <i class="far fa-comment hover:bg-blue-50 rounded-full p-2"></i>
-                    <span class="ml-1 text-sm">11</span>
-                  </div>
-                  <div class="text-gray-500 hover:text-green-500">
-                    <i class="fas fa-retweet hover:bg-green-50 rounded-full p-2"></i>
-                    <span class="ml-1 text-sm">11</span>
-                  </div>
-                  <div class="text-gray-500 hover:text-red-500">
-                    <i class="far fa-heart hover:bg-red-50 rounded-full p-2"></i>
-                    <span class="ml-1 text-sm">11</span>
-                  </div>
-                  <div class="text-gray-500 hover:text-primary">
-                    <i class="far fa-share-square hover:bg-blue-50 rounded-full p-2"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- 트렌드 섹션-->
-        <div class="w-2/5">trend</div>
-      </div>
+    <div class="flex-1 flex h-screen">
+        <messages />
     </div>
     
   </div>
 </template>
 
 <script>
-export default {
+import Messages from './pages/Messages.vue'
 
+export default {
+  components : { Messages},
+  setup(){},
+    
 }
 </script>
 
---
+++ TwiterVue3/src/pages/Messages.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex-1 flex">
+    <!-- 채팅리스트-->
+    <div class="w-2/5 border-r border-gray-100 overflow-y-scroll">
+        <div class="flex flex-col">
+            <div class="p-3 font-bold text-lg border-b border-gray-100">쪽지</div>
+            <!-- 채팅-->
+            <div class="flex px-3 py-4 hover:bg-gray-50 border-b border-gray-100"
+            v-for="message in 5" :key="message">
+                <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full cursor-pointer mr-2"/>
+                <div>
+                    <div class="flex space-x-1">
+                        <div class="font-bold">김수진</div>
+                        <div class="text-gray-500">sujin123</div>
+                        <div class="text-gray-500">2월27일</div>
+                    </div>
+                    <div class="text-gray-500">메시지</div>
+                </div>
+            </div>
+            <div class="flex px-3 py-4 hover:bg-gray-200 border-b ">
+                <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full cursor-pointer mr-2"/>
+                <div>
+                    <div class="flex space-x-1">
+                        <div class="font-bold">김수진</div>
+                        <div class="text-gray-500">sujin123</div>
+                        <div class="text-gray-500">2월27일</div>
+                    </div>
+                    <div class="text-gray-500">메시지</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- chatting-->
+    <div class="w-3/5 border-r border-gray-100">
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
