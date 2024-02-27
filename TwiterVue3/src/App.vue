ae7abfa0cafd7726e52d6e845b72da41de13c543
JeaHwanLim <dlawoghks5708@gmail.com>
2024-02-27 13:06:42 +0900

    사이드 바 완성

---
+++ TwiterVue3/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="flex h-screen">
+    <!-- 사이드 섹션-->
+    <div class="w-1/4 pt-5 ml-10 flex flex-col justify-between border-r border-gray-200">
+      <div class="flex flex-col items-center lg:items-start">
+        <!-- 트위터 로그-->
+        <i class="fa-brands fa-twitter text-2xl text-primary lg:ml-4 mb-3"></i>
+        <!-- 트위터 사이드메뉴 아이콘-->
+        <div class="flex flex-col items-start">
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+            <i class="fa-solid fa-house fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">홈</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+            <i class="fa-solid fa-hashtag fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">탐색하기</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+            <i class="far fa-bell fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">알림</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+            <i class="far fa-envelope fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">쪽지</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+            <i class="far fa-bookmark fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">북마크</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+            <i class="far fa-list-alt fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">리스트</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+            <i class="far fa-user fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">프로필</span>
+          </div>
+          <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+            <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">더보기</span>
+          </div>
+        </div>
+        <!-- 트윕버튼-->
+        <div class="w-full lg:pr-3 flex justify-center">
+          <button class="mt-3 bg-primary text-white lg:w-full w-12 h-12 rounded-full hover:bg-dark">
+            <span class="hidden lg:block">트윗</span>
+            <i class="fas fa-plus lg:hidden"></i>
+          </button>
+        </div>
+      </div>
+      <!-- 프로필 버튼-->
+      <div class="lg:pr-3 mb-3">
+          <button class="hidden lg:flex mt-3 px-2 py-1 w-full h-12 rounded-full hover:bg-blue-50 flex items-center">
+            <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full"/>
+            <div class="lg:ml-2 hidden lg:block">
+              <div class="text-sm font-bold">dlawoghks.com</div>
+              <div class="text-xs text-gray-500 text-left">@dlawoghks</div>
+            </div>
+            <i class="ml-auto fas fa-ellipsis-h fa-fw text-xs hidden lg:block"></i>
+          </button>
+          <div class="lg:hidden flex justify-center">
+            <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full cursor-pointer"/>
+          </div>
+      </div>
+    </div>
+    <div class="flex-1 ">
+      
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
